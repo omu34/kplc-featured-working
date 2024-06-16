1365ed2f9d440d4043935fd02e4cc8b7208d2a19
bernard omuse <bernardomuse22@gmail.com>
2024-06-16 14:06:03 +0300

    media specificity

--- resources/views/livewire/featured-items.blade.php
+++ resources/views/livewire/featured-items.blade.php
@@ -5,7 +5,7 @@
             </div>
 
             <div
-                class="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-4 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+                class="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-4 lg:mx-0 lg:max-w-none lg:grid-cols-4 nextItems">
                 @foreach ($currentItems as $currentItem)
                     <article data-aos="fade-up" data-aos-duration="1500"
                         class="relative isolate flex flex-col transition-transform transform hover:scale-105 justify-end overflow-hidden rounded-2xl bg-gray-900 px-4 pb-8 pt-44 sm:pt-60 lg:pt-48">
